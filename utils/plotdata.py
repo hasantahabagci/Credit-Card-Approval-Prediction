6d5dbbe1bb374c2b8493142b7a02b41e0d5d1ba7
Yusuf Faruk Guldemir <100127031+yusufarukguld@users.noreply.github.com>
2023-12-30 19:29:04 +0300

    plotdata.py utils file is updated

--- utils/plotdata.py
+++ utils/plotdata.py
@@ -11,9 +11,11 @@ def plotdist(df, col, mx):
     :return: None
     """
     plt.figure(figsize=(10, 6))
-    sns.distplot(df[col], kde=True)
+    sns.distplot(df[col], kde=True, label='skewness : %.2f'%(df[col].skew()))
     plt.vlines(df[col].mean(), 0, mx, colors='r', label='mean')
     plt.text(df[col].mean(), mx*1.01, "mean : "+ str(round(df[col].mean(),2)), fontsize=12)
+    plt.title(col.upper() + " DISTRIBUTION")
+    plt.legend()
     plt.show()
 
 def plotbox(df, col):
@@ -25,4 +27,17 @@ def plotbox(df, col):
     """
     plt.figure(figsize=(10, 6))
     sns.boxplot(df[col])
+    plt.show()
+
+def plotbar(df, col):
+    """
+    This function plots the barplot of a column
+    :param df: dataframe
+    :param col: column name
+    :return: None
+    """
+    plt.figure(figsize=(10, 6))
+    sns.countplot(df[col])
+    plt.title(col.upper() + " Bar Plot")
+    plt.xticks(rotation=90)
     plt.show()
